74cc144f0e17ff741da84aa347b656b104cb2838
Emanuel1308 <emanuelalbuquerque16@gmail.com>
2022-04-06 13:40:13 -0300

    Sintaxe básica

---
+++ JavaScript/Sintáxe básica - JavaScript/Aula 5 - funções/script.js
@@ -0,0 +1,30 @@
+/*function funcao() {
+  console.log('Essa mensagem veio de uma função')
+}
+
+funcao()
+
+function mensagem(primeiro, segundo) {
+  console.log(primeiro, segundo)
+}
+
+mensagem('Tudo certo', 'Jovem')
+*/
+
+//Função declarativas
+
+//Expressões de Funções.
+
+/*var funcao = function () {
+  console.log('Sou uma mensagem de função expressão')
+}
+funcao()
+*/
+
+//arrow function
+
+/*var funcaoA = () => {
+  console.log('sou uma arrow function')
+}
+funcaoA()
+*/
